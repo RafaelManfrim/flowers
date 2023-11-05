b3e8a11581d0addcf796e0d9852e7d9560b8f759
Rafael Manfrim <rafaelmanfrim2004@gmail.com>
2023-11-04 23:07:11 -0300

    Fazendo melhorias visuais

--- flowers/main.js
+++ flowers/main.js
@@ -2,9 +2,8 @@ onload = () =>{
         document.body.classList.remove("container");
 
         setTimeout(() => {
-                const background = document.querySelector(".night")
                 const textSpan = document.createElement("span")
-                textSpan.innerHTML = "Eu te amo, Ana Rita!"
-                background.appendChild(textSpan)
+                textSpan.innerHTML = "Eu te amo, Ana Rita <3"
+                document.body.appendChild(textSpan)
         }, [1500])
 };
