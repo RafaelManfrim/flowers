e8b13a9eb1f76a5a05621408a2203a5777ab67aa
Rafael Manfrim <rafaelmanfrim2004@gmail.com>
2023-11-04 23:00:20 -0300

    Testando texto customizado

--- flowers/main.js
+++ flowers/main.js
@@ -1,3 +1,10 @@
 onload = () =>{
         document.body.classList.remove("container");
+
+        setTimeout(() => {
+                const background = document.querySelector(".night")
+                const textSpan = document.createElement("span")
+                textSpan.innerHTML = "Eu te amo, Ana Rita!"
+                background.appendChild(textSpan)
+        }, [1500])
 };
